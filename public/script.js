b464be41107ca62f432060e45d87714f1dfd492d
Manmohan <manmohanjangra2528@gmail.com>
2023-12-18 00:41:22 +0530

    Popup Done

--- public/script.js
+++ public/script.js
@@ -1,3 +1,27 @@
-$('#email-form').on('submit', (event) => {
+$(document).ready(function () {
+  setTimeout(() => {
+    $(".modal-wrapper").fadeIn();
+  }, 500);
+
+  $("#email-form").on("submit", (event) => {
     event.preventDefault();
-})
+
+    const email = $("#email").val();
+    if (email.length == 0 || !email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+        $("#email-form p").removeClass("success");
+        $("#email-form p").addClass("error");
+        $("#email-form p").html("Please enter your email address");
+      return;
+    } else {
+        $("#email-form p").removeClass("error");
+        $("#email-form p").addClass("success");
+        $('.initial-popup').hide();
+        $('.final-popup').show();
+    }
+    $("#email-form p").html("");
+  });
+
+  $(".modal-close").on("click", () => {
+    $(".modal-wrapper").fadeOut();
+  });
+});
